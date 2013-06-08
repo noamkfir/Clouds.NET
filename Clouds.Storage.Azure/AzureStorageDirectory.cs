ac5d998aa876212306244ac7ff86d3682294a377
JonGonard <yonatan.horovitz@gmail.com>
2013-06-08 17:34:29 +0300

    Azure storage plugin, not working yet

---
+++ Clouds.Storage.Azure/AzureStorageDirectory.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Clouds.Common.Storage.Interfaces;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Clouds.Storage.Azure
+{
+    public class AzureStorageDirectory : IStorageDirectory
+    {
+        private readonly CloudBlobContainer _container;
+        private readonly CloudBlobDirectory _directory;
+
+        public AzureStorageDirectory(CloudBlobContainer container) : this(container, null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AzureStorageDirectory(CloudBlobContainer container, CloudBlobDirectory directory)
+        {
+            _container = container;
+            _directory = directory;
+            Uri = directory != null ? directory.Uri : container.Uri;
+            Name = directory != null
+                       ? directory.Uri.Segments[directory.Uri.Segments.Length - 1]
+                       : container.Name;
+        }
+
+        public Uri Uri { get; private set; }
+        public string Name { get; private set; }
+
+        public IStorageDirectory GetParent()
+        {
+            if (_directory != null)
+            {
+                return new AzureStorageDirectory(_container, _directory.Parent);
+            }
+
+            return null;
+        }
+
+        public IStorageDirectory GetRoot()
+        {
+            return new AzureStorageDirectory(_container.ServiceClient.GetRootContainerReference());
+        }
+
+        public IEnumerable<IStorageDirectory> GetDirectories()
+        {
+            if (_directory != null)
+                return
+                    _directory.ListBlobs()
+                              .OfType<CloudBlobDirectory>()
+                              .Select(blob => new AzureStorageDirectory(_container, blob));
+
+            return _container.ListBlobs()
+                             .OfType<CloudBlobDirectory>()
+                             .Select(blob => new AzureStorageDirectory(_container, blob));
+        }
+
+        public IEnumerable<IStorageFile> GetFiles()
+        {
+            if (_directory != null)
+                return
+                    _directory.ListBlobs()
+                              .OfType<CloudBlockBlob>()
+                              .Select(blob => new AzureStorageFile(_container, blob));
+
+            return _container.ListBlobs()
+                             .OfType<CloudBlockBlob>()
+                             .Select(blob => new AzureStorageFile(_container, blob));
+        }
+    }
+}
--- Clouds.Storage.Azure/AzureStorageProvider.cs
+++ Clouds.Storage.Azure/AzureStorageProvider.cs
@@ -10,15 +10,28 @@ namespace Clouds.Storage.Azure
     public class AzureStorageProvider : IStorageProvider
     {
         private readonly CloudStorageAccount _account;
+        private readonly IStorageDirectory _rootDir;
 
         public AzureStorageProvider(string connectionString)
         {
             _account = CloudStorageAccount.Parse(connectionString);
+
+            var client = _account.CreateCloudBlobClient();
+
+            _rootDir = new AzureStorageDirectory(client.GetRootContainerReference());
         }
 
-        public Task<IStorageDirectory> CreateDirectory(IStorageDirectory parent, string name)
+        public Task<IStorageDirectory> GetDirectory(Uri uri)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<IStorageDirectory> CreateDirectory(IStorageDirectory parent, string name)
+        {
+            var newName = name + "/";
+
+            await CreateFile(parent, newName, new MemoryStream(0));
+            return await GetDirectory(new Uri(parent.Uri + newName));
         }
 
         public async Task CreateFile(IStorageDirectory directory, string name, Stream file)
@@ -33,6 +46,8 @@ namespace Clouds.Storage.Azure
                                              new BlobContainerPermissions {PublicAccess = BlobContainerPublicAccessType.Blob},
                                              null);
 
+                CloudBlobDirectory d;
+
                 CloudBlockBlob blob = container.GetBlockBlobReference(name);
 
                 using (file)
@@ -40,11 +55,6 @@ namespace Clouds.Storage.Azure
                     await Task.Factory.FromAsync(blob.BeginUploadFromStream, blob.EndUploadFromStream, file, null);
                 }
             }
-        }
-
-        public Task<IStorageDirectory> GetDirectory(Uri uri)
-        {
-            throw new NotImplementedException();
         }
     }
 }
